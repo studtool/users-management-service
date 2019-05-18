fef5381dd64bec8be52419d89d32600d75e54bd8
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-05-18 18:30:57 +0300

    main() implementation update

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/mailru/easyjson v0.0.0-20190403194419-1ea4449da983
 	github.com/streadway/amqp v0.0.0-20190404075320-75d898a42a94
 	github.com/studtool/common v0.0.0-20190518150158-798c0b8db3ef
+	go.uber.org/dig v1.7.0
 )
--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"os"
+	"os/signal"
 
+	"go.uber.org/dig"
+
+	"github.com/studtool/common/utils"
+
+	"github.com/studtool/users-management-service/beans"
+	"github.com/studtool/users-management-service/messages"
+)
+
+func main() {
+	c := dig.New()
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, os.Kill)
+
+	utils.AssertOk(c.Provide(messages.NewQueueClient))
+	utils.AssertOk(c.Invoke(func(q *messages.QueueClient) {
+		if err := q.OpenConnection(); err != nil {
+			beans.Logger.Fatal(err)
+		} else {
+			beans.Logger.Info("queue: connection open")
+		}
+		if err := q.Run(); err != nil {
+			beans.Logger.Fatal(err)
+		} else {
+			beans.Logger.Info("queue: ready to receive messages")
+		}
+	}))
+	defer func() {
+		utils.AssertOk(c.Invoke(func(q *messages.QueueClient) {
+			if err := q.CloseConnection(); err != nil {
+				beans.Logger.Fatal(err)
+			} else {
+				beans.Logger.Info("queue: connection closed")
+			}
+		}))
+	}()
+
+	<-ch
 }
