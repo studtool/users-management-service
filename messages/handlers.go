d72977a05336fe1a4073149046052b034ad729ad
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-05-18 19:17:24 +0300

    some logs added

--- messages/handlers.go
+++ messages/handlers.go
@@ -1,15 +1,7 @@
 package messages
 
 import (
-	"fmt"
-
-	"github.com/mailru/easyjson"
-	"github.com/streadway/amqp"
-
-	"github.com/studtool/common/consts"
 	"github.com/studtool/common/queues"
-
-	"github.com/studtool/users-management-service/beans"
 )
 
 func (c *MqClient) handleUserCreated(body []byte) {
@@ -19,6 +11,8 @@ func (c *MqClient) handleUserCreated(body []byte) {
 	} else {
 		c.publishProfileToCreateMessage(createdUserData)
 		c.publishDocumentUserToCreateMessage(createdUserData)
+
+		c.writeMessageConsumedLog(c.createdUsersQueue.Name, createdUserData)
 	}
 }
 
@@ -29,6 +23,8 @@ func (c *MqClient) handleUserDeleted(body []byte) {
 	} else {
 		c.publishProfileToDeleteMessage(deletedUserData)
 		c.publishDocumentUserToDeletedMessage(deletedUserData)
+
+		c.writeMessageConsumedLog(c.createdUsersQueue.Name, deletedUserData)
 	}
 }
 
@@ -70,51 +66,4 @@ func (c *MqClient) publishDocumentUserToDeletedMessage(
 			UserID: deletedUserData.UserID,
 		},
 	)
-}
-
-func (c *MqClient) publishMessage(
-	queueName string,
-	data easyjson.Marshaler,
-) {
-	body, err := c.marshalMessageBody(data)
-
-	err = c.channel.Publish(
-		consts.EmptyString,
-		queueName,
-		false,
-		false,
-		amqp.Publishing{
-			Body:        body,
-			ContentType: "application/json",
-		},
-	)
-	if err == nil {
-		c.writeMessagePublishedLog(queueName, data)
-	} else {
-		c.writeMessagePublicationErrorLog(queueName, data)
-	}
-}
-
-func (c *MqClient) writeMessagePublishedLog(
-	queueName string,
-	data easyjson.Marshaler,
-) {
-	beans.Logger.Info(
-		fmt.Sprintf("message published (%s): %v", queueName, data),
-	)
-}
-
-func (c *MqClient) writeMessagePublicationErrorLog(
-	queueName string,
-	data easyjson.Marshaler,
-) {
-	beans.Logger.Error(
-		fmt.Sprintf("message not published (%s): %v", queueName, data),
-	)
-}
-
-func (c *MqClient) writeErrorLog(err error) {
-	if err != nil {
-		beans.Logger.Error(err)
-	}
 }
---
+++ messages/utils.go
@@ -0,0 +1,77 @@
+package messages
+
+import (
+	"fmt"
+
+	"github.com/mailru/easyjson"
+	"github.com/streadway/amqp"
+
+	"github.com/studtool/common/consts"
+
+	"github.com/studtool/users-management-service/beans"
+)
+
+func (c *MqClient) publishMessage(
+	queueName string,
+	data easyjson.Marshaler,
+) {
+	body, err := c.marshalMessageBody(data)
+
+	err = c.channel.Publish(
+		consts.EmptyString,
+		queueName,
+		false,
+		false,
+		amqp.Publishing{
+			Body:        body,
+			ContentType: "application/json",
+		},
+	)
+	if err == nil {
+		c.writeMessagePublishedLog(queueName, data)
+	} else {
+		c.writeMessagePublicationErrorLog(queueName, data)
+	}
+}
+
+func (c *MqClient) writeMessagePublishedLog(
+	queueName string,
+	data easyjson.Marshaler,
+) {
+	beans.Logger.Info(
+		fmt.Sprintf("message published (%s): %v", queueName, data),
+	)
+}
+
+func (c *MqClient) writeMessagePublicationErrorLog(
+	queueName string,
+	data easyjson.Marshaler,
+) {
+	beans.Logger.Error(
+		fmt.Sprintf("message not published (%s): %v", queueName, data),
+	)
+}
+
+func (c *MqClient) writeMessageConsumedLog(
+	queueName string,
+	data easyjson.Marshaler,
+) {
+	beans.Logger.Info(
+		fmt.Sprintf("message consumed (%s): %v", queueName, data),
+	)
+}
+
+func (c *MqClient) writeMessageConsumptionErrorLog(
+	queueName string,
+	data easyjson.Marshaler,
+) {
+	beans.Logger.Error(
+		fmt.Sprintf("message not consumed (%s): %v", queueName, data),
+	)
+}
+
+func (c *MqClient) writeErrorLog(err error) {
+	if err != nil {
+		beans.Logger.Error(err)
+	}
+}
